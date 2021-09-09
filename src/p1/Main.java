3858cf9c75852eb23c11e81ba65a7fcb90c8efa7
Ruben <rubenvanrooijen23@outlook.com>
2021-09-09 10:47:40 +0200

    net afwerken tussenvoegsel!

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -11,12 +11,16 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
         try{
-            Connection conn = DriverManager.getConnection("jdbc:postgresql:voorbereiding", "postgres", "ruben");
+            Connection conn = DriverManager.getConnection("jdbc:postgresql:DataPersistency", "postgres", "ruben");
             Statement stmt = conn.createStatement();
             ResultSet resultSet = stmt.executeQuery("SELECT * FROM reiziger");
             System.out.println("Alle reizigers:");
             while (resultSet.next()){
-                System.out.println("\t #" + resultSet.getString("reiziger_id") + ": " + resultSet.getString("voorletters") + " " + resultSet.getString("tussenvoegsel") + " " + resultSet.getString("achternaam") + "  (" + resultSet.getString("geboortedatum") + ")");
+                if (resultSet.getString("tussenvoegsel") == null){
+                    System.out.println("\t #" + resultSet.getString("reiziger_id") + ": " + resultSet.getString("voorletters") + " " + resultSet.getString("achternaam") + "  (" + resultSet.getString("geboortedatum") + ")");
+                }else{
+                    System.out.println("\t #" + resultSet.getString("reiziger_id") + ": " + resultSet.getString("voorletters") + " " + resultSet.getString("tussenvoegsel") + " " + resultSet.getString("achternaam") + "  (" + resultSet.getString("geboortedatum") + ")");
+                }
             }
 
             conn.close();
